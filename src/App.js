7bee47d42ef068333b7d571472ef861d20c149fd
Mayank Kumar Sharma <mayanksharma6979@gmail.com>
2023-06-13 10:24:53 +0530

    Learning react

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let a = 3453;
   return (
     <div className="App">
       <header className="App-header">
+        <div> {a}</div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          // href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
