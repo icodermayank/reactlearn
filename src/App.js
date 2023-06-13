a57152af974ce45097890bbade0b47e4432990bf
Mayank Kumar Sharma <mayanksharma6979@gmail.com>
2023-06-13 10:36:05 +0530

    Bootstrap included

--- src/App.js
+++ src/App.js
@@ -4,23 +4,11 @@ import './App.css';
 function App() {
   let a = 3453;
   return (
-    <div className="App">
-      <header className="App-header">
-        <div> {a}</div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          // href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>React app</h1>
+    <p>This is my first react app</p>
+
+    </>
   );
 }
 
